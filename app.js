31ca0026247171254f80e793ca9e16b1a96a3099
mike <angelrosillo2018@gmail.com>
2018-10-17 12:35:12 -0500

    codeadded

---
+++ app.js
@@ -0,0 +1,125 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+//var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+/*mongoose.connect('mongodb://localhost/loginapp',{ useNewUrlParser: true });
+.then(db => console.log("DB is connected mike is awesome"))
+.catch(err => console.error(err));*/
+const { mongoose } = require("./bin/database.js");
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // Set public folder as root
+// app.use(express.static('public'));
+
+// Provide access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+// // Redirect all traffic to index.html
+// app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+// Make sure to include the JSX transpiler
+//require('node-jsx').install();
+
+//setup routes for the app
+//require('./src/routes/core-routes.js')(app);
+
+app.use('/', routes);
+app.use('/users', users);
+
+//Route not found -- Set 404
+/*app.get('*', function(req, res) {
+    res.json({
+        'route': 'Sorry this page does not exist!'
+    });
+});*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
