27b3d2a9bbb21e6154117976a0e761c9e2c3749f
mike <angelrosillo2018@gmail.com>
2018-10-22 14:48:37 -0500

    server refactored

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+var debug = require('debug')('liveconnect:server');
+var http = require('http');
+
 
 const { url } = require('./config/database.js');
 
@@ -18,7 +21,7 @@ mongoose.connect(url, {
 
 require('./config/passport.js')(passport);
 
-const port = 3000;
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -50,7 +53,65 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to main.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/main.html`));
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.info('listening on %d', port);
-});
+
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port' , port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error' , onError);
+server.on('listening' , onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
